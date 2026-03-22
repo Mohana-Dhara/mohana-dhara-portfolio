64f48061ad20ce7c6d5bd49f9ab26fdbf4a23be3
Mohana-dhara <mohanavamsi611@gmail.com>
2026-03-22 19:46:01 -0400

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -133,7 +133,7 @@ export const Hero = () => {
               />
               <div className="relative glass rounded-3xl p-2 glow-border">
                 <img
-                  src= "/profile.png"
+                  src= "/gradphoto.png"
                   alt="Mohana Dhara"
                   className="w-full aspect-[4/5] object-cover rounded-2xl"
                 />
